d23889adf737dc864133f21e7ce4db53e7650415
Sahas Prajapati <b10prajapati@gmail.com>
2021-02-13 21:34:05 +0545

    Preferred movie list

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../model/User");
+const MongoClient = require("mongodb").MongoClient;
 
 router.get("/", auth, async (req, res) => {
   try {
@@ -14,15 +15,52 @@ router.get("/", auth, async (req, res) => {
     console.error(err);
     res.status(500).send("Server Error");
   }
+  const db = client.db("Movie");
 });
 
-router.get("/preferred-movies", auth, async (req, res) => {
+const uri =
+  "mongodb+srv://nirjal123:nirjal123@cluster0.6jptv.mongodb.net/Movie?retryWrites=true&w=majority";
+const findMovie = async (list) => {
+  let allValues;
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).catch((err) => console.log(err));
+
+  if (!client) {
+    return;
+  }
+
+  try {
+    const db = client.db("Movie");
+    const MongoClient = require("mongodb").MosngoClient;
+
+    const collection = db.collection("Movie");
+
+    let cursor = collection.find({ index: { $in: list } });
+
+    // for await (const doc of cursor) {
+    //   console.log(doc);
+    // }
+    allValues = await cursor.toArray();
+
+    // console.log(allValues);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.close();
+  }
+  return allValues;
+};
+
+router.get("/preferred_movies", auth, async (req, res) => {
   try {
     const movieList = await User.findOne(
       { _id: req.user.id },
       { _id: 0, preferredMovies: 1 }
     );
-    res.json(movieList);
+    let movie = await findMovie(movieList.preferredMovies);
+    res.json(movie);
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
